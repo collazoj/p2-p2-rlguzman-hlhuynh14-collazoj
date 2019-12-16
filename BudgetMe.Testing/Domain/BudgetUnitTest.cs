d46d5598dba981e30d2c316ab4d2efcafab0890d
Roberto <rlguzman.developer@gmail.com>
2019-12-16 12:39:42 -0800

    WebApi and unit test

--- BudgetMe.Testing/Domain/BudgetUnitTest.cs
+++ BudgetMe.Testing/Domain/BudgetUnitTest.cs
@@ -1,21 +1,22 @@
 using BudgetMe.Domain.Controllers;
+using BudgetMe.Domain.Models;
 using Xunit;
 
 namespace BudgetMe.Testing.Domain
 {
   public class BudgetUnitTest
   {
-    // [Theory]
-    // [InlineData("Bob", 1000)]
-    // public async void Test_CreateIncome(string name, int amount)
-    // {
-    // // arrange 
-    //   var sut = new BudgetController();
-    // // act
-    //   var actual = await sut.CreateIncome(name, amount);
-    // // assert
-    //   Assert.True(actual.ToString() == "200");
-    // }
+    [Theory]
+    [InlineData("Bob", 1000)]
+    public async void Test_CreateIncome(string name, int amount)
+    {
+    // arrange 
+      var sut = new BudgetController();
+    // act
+      var actual = await sut.CreateIncome(name, amount);
+    // assert
+      Assert.IsType<Income>(actual);
+    }
   //  [Theory]
   //   [InlineData(1)]
   //   [InlineData(2)]
