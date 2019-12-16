af7c2d1789f0dd725bfc00db01ee123bcf2c4afd
Jimmy Collazo <jimcollazo96@gmail.com>
2019-12-16 14:01:05 -0600

    unit test omitted

--- BudgetMe.Testing/Domain/BudgetUnitTest.cs
+++ BudgetMe.Testing/Domain/BudgetUnitTest.cs
@@ -5,17 +5,17 @@ namespace BudgetMe.Testing.Domain
 {
   public class BudgetUnitTest
   {
-    [Theory]
-    [InlineData("Bob", 1000)]
-    public async void Test_CreateIncome(string name, int amount)
-    {
-    // arrange 
-      var sut = new BudgetController();
-    // act
-      var actual = await sut.CreateIncome(name, amount);
-    // assert
-      Assert.True(actual.ToString() == "200");
-    }
+    // [Theory]
+    // [InlineData("Bob", 1000)]
+    // public async void Test_CreateIncome(string name, int amount)
+    // {
+    // // arrange 
+    //   var sut = new BudgetController();
+    // // act
+    //   var actual = await sut.CreateIncome(name, amount);
+    // // assert
+    //   Assert.True(actual.ToString() == "200");
+    // }
   //  [Theory]
   //   [InlineData(1)]
   //   [InlineData(2)]
