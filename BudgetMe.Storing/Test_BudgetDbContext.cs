18ffc865a2249580f98a9316a54b17c562c3a03c
Jimmy Collazo <jimcollazo96@gmail.com>
2019-12-17 15:48:04 -0600

    deleted all Domain models, TestDbContext should pass

--- BudgetMe.Storing/Test_BudgetDbContext.cs
+++ BudgetMe.Storing/Test_BudgetDbContext.cs
@@ -18,10 +18,7 @@ namespace BudgetMe.Storing
         
         builder.Entity<Member>().Property(p => p.Id).UseSerialColumn();
 
-        builder.Entity<Member>().HasData(new List<Member>()
-        {
-          new Member(){Id = 1, FirstName = "Jimmy", LastName ="C"},
-        });
+        builder.Entity<Member>().HasData(MakeSeedMembers());
 
         builder.Entity<Budget>(o => o.HasKey(k => k.Id));
 
@@ -31,6 +28,16 @@ namespace BudgetMe.Storing
         {
           new Budget(){Id = 1, Name = "jimmybudget", MemberId=1, TotalMonthlyNetIncome=0, RemainderAfterBill=0, RemainderAfterGoals=0, Percent=0, RemainderAfterExpenses=0}
         });
+      }
+
+      public List<Member> MakeSeedMembers()
+      {
+        return new List<Member>(){
+          new Member(){Id = 1, FirstName = "Jimmy", LastName ="Collazo"},
+          new Member(){Id = 2, FirstName = "Henry", LastName="Huynh"},
+          new Member(){Id = 3, FirstName = "John", LastName="Harvey"},
+          new Member(){Id = 4, FirstName = "Roberto", LastName="Guzman"}
+        };
       }
     }
 }
--- BudgetMe.Testing/Storing/Test_OrmAdapter.cs
+++ BudgetMe.Testing/Storing/Test_OrmAdapter.cs
@@ -9,6 +9,37 @@ namespace BudgetMe.Testing.Storing
 {
     public class Test_OrmAdapter
     {
+      [Fact]
+      public void Test_TestDbContext()
+      {
+        //Arrange
+        Test_BudgetDbContext tdb = new Test_BudgetDbContext();
+        tdb.Database.EnsureCreated();
+
+        //Act
+        var members = tdb.Member;
+
+        //Assert
+        Assert.True(members.Count()!=0); //Should be 4. Passes.
+      }
+
+      // [Fact]
+      // public void TestOrmAdapter_Insert()
+      // {
+      //   //Arrange
+      //   Test_BudgetDbContext tdb = new Test_BudgetDbContext();
+      //   OrmAdapter<Test_BudgetDbContext> oa = new OrmAdapter<Test_BudgetDbContext>(tdb);
+
+      //   //Act
+      //   Member m = new Member(){Id=10, FirstName="Henry", LastName="H"};
+      //   bool a = oa.InsertMember(m);
+
+      //   List<Member> memberlist = oa.GetMembers();
+      //   Member member = memberlist.Where(m => m.Id == 10).SingleOrDefault();
+
+      //   //Assert
+      //   Assert.NotNull(member);
+      // }
 
     }
 }
