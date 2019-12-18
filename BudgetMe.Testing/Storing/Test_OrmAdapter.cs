df4fe4816f92fe28a95597ff0ace40a59872dcd3
Jimmy Collazo <jimcollazo96@gmail.com>
2019-12-17 20:17:17 -0600

    4 tests for OrmAdapter

--- BudgetMe.Testing/Storing/Test_OrmAdapter.cs
+++ BudgetMe.Testing/Storing/Test_OrmAdapter.cs
@@ -9,37 +9,89 @@ namespace BudgetMe.Testing.Storing
 {
     public class Test_OrmAdapter
     {
+
       [Fact]
       public void Test_TestDbContext()
       {
         //Arrange
-        Test_BudgetDbContext tdb = new Test_BudgetDbContext();
-        tdb.Database.EnsureCreated();
+        Test_BudgetDbContext _tdb = new Test_BudgetDbContext();
+        _tdb.Database.EnsureCreated();
+        OrmAdapter<Test_BudgetDbContext> _oa = new OrmAdapter<Test_BudgetDbContext>(_tdb);
+        _oa.InsertMember(new Member(){Id = 120, FirstName="Fred", LastName="B"});
+
 
         //Act
-        var members = tdb.Member;
+        var members = _tdb.Member.ToList();
 
         //Assert
         Assert.True(members.Count()!=0); //Should be 4. Passes.
       }
 
+      [Fact]
+      public void TestOrmAdapter_Insert()
+      {
+        //Arrange
+        Test_BudgetDbContext _tdb = new Test_BudgetDbContext();
+        OrmAdapter<Test_BudgetDbContext> _oa = new OrmAdapter<Test_BudgetDbContext>(_tdb);
+
+        //Act
+        bool insertmemberbool = _oa.InsertMember(new Member(){Id = 20, FirstName="Fred", LastName="B"});
+        Member member = _tdb.Member.Where(m => m.Id == 20).SingleOrDefault();
+        string last = member.LastName;
+        _oa.RemoveMember(20);
+
+        //Assert
+        Assert.True(last == "B");
+      }
+
+      [Fact]
+      public void TestOrmAdapter_Get()
+      {
+        Test_BudgetDbContext _tdb = new Test_BudgetDbContext();
+        
+        OrmAdapter<Test_BudgetDbContext> _oa = new OrmAdapter<Test_BudgetDbContext>(_tdb);
+        _oa.InsertMember(new Member(){FirstName="Fred", LastName="B"});
+        List<Member> members = _oa.GetMembers();
+
+        Assert.True(members.Count!=0);
+      }
+
       // [Fact]
-      // public void TestOrmAdapter_Insert()
+      // public void TestOrmAdapter_Update()
       // {
-      //   //Arrange
-      //   Test_BudgetDbContext tdb = new Test_BudgetDbContext();
-      //   OrmAdapter<Test_BudgetDbContext> oa = new OrmAdapter<Test_BudgetDbContext>(tdb);
+      //   Test_BudgetDbContext _tdb = new Test_BudgetDbContext();
+      //   _tdb.Database.EnsureCreated();
+      //   OrmAdapter<Test_BudgetDbContext> _oa = new OrmAdapter<Test_BudgetDbContext>(_tdb);
+        
+      //   Member member = _tdb.Member.Where(m => m.Id == 1).SingleOrDefault();
+      //   member.FirstName = "Brad";
+      //   member.LastName = "Pitt";
+      //   _oa.UpdateMember(member);
 
-      //   //Act
-      //   Member m = new Member(){Id=10, FirstName="Henry", LastName="H"};
-      //   bool a = oa.InsertMember(m);
+      //   Member mem = _tdb.Member.Where(m => m.LastName == "Pitt").SingleOrDefault();
+      //   string newFirstName = mem.FirstName;
 
-      //   List<Member> memberlist = oa.GetMembers();
-      //   Member member = memberlist.Where(m => m.Id == 10).SingleOrDefault();
+      //   Member member1 = _tdb.Member.Where(m => m.Id == 1).SingleOrDefault();
+      //   member1.FirstName = "Jimmy";
+      //   member1.LastName = "Collazo";
+      //   _oa.UpdateMember(member1);
 
-      //   //Assert
-      //   Assert.NotNull(member);
+      //   Assert.True(newFirstName=="Brad");
       // }
+
+      [Fact]
+      public void TestOrmAdapter_Remove()
+      {
+        Test_BudgetDbContext _tdb = new Test_BudgetDbContext();
+        OrmAdapter<Test_BudgetDbContext> _oa = new OrmAdapter<Test_BudgetDbContext>(_tdb);
+        _oa.InsertMember(new Member(){Id = 21, FirstName="Fred", LastName="B"});
+        int MemberCount = _oa.GetMembers().Count();
+
+        _oa.RemoveMember(21);
+        int newMemberCount = _oa.GetMembers().Count();
+
+        Assert.True(newMemberCount==MemberCount-1);
+      }
 
     }
 }
