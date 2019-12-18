656f91de8ecfb8b530f9e048fc381b5fca6579b4
Jimmy Collazo <jimcollazo96@gmail.com>
2019-12-17 20:52:06 -0600

    5 tests run for OrmAdapter. Had to EnsureCreate() and EnsureDelete()

--- BudgetMe.Testing/Storing/Test_OrmAdapter.cs
+++ BudgetMe.Testing/Storing/Test_OrmAdapter.cs
@@ -17,11 +17,10 @@ namespace BudgetMe.Testing.Storing
         Test_BudgetDbContext _tdb = new Test_BudgetDbContext();
         _tdb.Database.EnsureCreated();
         OrmAdapter<Test_BudgetDbContext> _oa = new OrmAdapter<Test_BudgetDbContext>(_tdb);
-        _oa.InsertMember(new Member(){Id = 120, FirstName="Fred", LastName="B"});
-
 
         //Act
         var members = _tdb.Member.ToList();
+        _tdb.Database.EnsureDeleted();
 
         //Assert
         Assert.True(members.Count()!=0); //Should be 4. Passes.
@@ -38,7 +37,8 @@ namespace BudgetMe.Testing.Storing
         bool insertmemberbool = _oa.InsertMember(new Member(){Id = 20, FirstName="Fred", LastName="B"});
         Member member = _tdb.Member.Where(m => m.Id == 20).SingleOrDefault();
         string last = member.LastName;
-        _oa.RemoveMember(20);
+        _tdb.Database.EnsureDeleted();
+
 
         //Assert
         Assert.True(last == "B");
@@ -48,36 +48,33 @@ namespace BudgetMe.Testing.Storing
       public void TestOrmAdapter_Get()
       {
         Test_BudgetDbContext _tdb = new Test_BudgetDbContext();
+        _tdb.Database.EnsureCreated();
         
         OrmAdapter<Test_BudgetDbContext> _oa = new OrmAdapter<Test_BudgetDbContext>(_tdb);
-        _oa.InsertMember(new Member(){FirstName="Fred", LastName="B"});
         List<Member> members = _oa.GetMembers();
+        _tdb.Database.EnsureDeleted();
 
         Assert.True(members.Count!=0);
       }
 
-      // [Fact]
-      // public void TestOrmAdapter_Update()
-      // {
-      //   Test_BudgetDbContext _tdb = new Test_BudgetDbContext();
-      //   _tdb.Database.EnsureCreated();
-      //   OrmAdapter<Test_BudgetDbContext> _oa = new OrmAdapter<Test_BudgetDbContext>(_tdb);
+      [Fact]
+      public void TestOrmAdapter_Update()
+      {
+        Test_BudgetDbContext _tdb = new Test_BudgetDbContext();
+        _tdb.Database.EnsureCreated();
+        OrmAdapter<Test_BudgetDbContext> _oa = new OrmAdapter<Test_BudgetDbContext>(_tdb);
         
-      //   Member member = _tdb.Member.Where(m => m.Id == 1).SingleOrDefault();
-      //   member.FirstName = "Brad";
-      //   member.LastName = "Pitt";
-      //   _oa.UpdateMember(member);
+        Member member = _tdb.Member.Where(m => m.Id == 1).SingleOrDefault();
+        member.FirstName = "Brad";
+        member.LastName = "Pitt";
+        _oa.UpdateMember(member);
 
-      //   Member mem = _tdb.Member.Where(m => m.LastName == "Pitt").SingleOrDefault();
-      //   string newFirstName = mem.FirstName;
+        Member mem = _tdb.Member.Where(m => m.LastName == "Pitt").SingleOrDefault();
+        string newFirstName = mem.FirstName;
+        _tdb.Database.EnsureDeleted();
 
-      //   Member member1 = _tdb.Member.Where(m => m.Id == 1).SingleOrDefault();
-      //   member1.FirstName = "Jimmy";
-      //   member1.LastName = "Collazo";
-      //   _oa.UpdateMember(member1);
-
-      //   Assert.True(newFirstName=="Brad");
-      // }
+        Assert.True(newFirstName=="Brad");
+      }
 
       [Fact]
       public void TestOrmAdapter_Remove()
@@ -89,6 +86,7 @@ namespace BudgetMe.Testing.Storing
 
         _oa.RemoveMember(21);
         int newMemberCount = _oa.GetMembers().Count();
+        _tdb.Database.EnsureDeleted();
 
         Assert.True(newMemberCount==MemberCount-1);
       }
